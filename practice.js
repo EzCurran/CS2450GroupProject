3e054bfea3284835c044dfe56d4fe8642c33fa87
Ezekial Curran <ecurran1@bruinmail.slcc.edu>
2022-06-03 03:13:18 -0600

    Created

---
+++ practice.js
@@ -0,0 +1,10 @@
+message = 'Hello World!';
+console.log('Variables');
+console.log('String example:');
+console.log(message);
+
+days = 7;
+months = 12;
+weeks = 52;
+console.log('Integers example:');
+console.log(days, months, weeks);
